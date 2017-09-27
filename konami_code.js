14d2fd05caca5f97b21c88134dc7209e9c6b6c4d
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-27 19:37:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,15 +2,15 @@ var i = 0;
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 //const input = document.body;
  function init(){
-   let res = 0; 
+   let res = 0;
 document.body.addEventListener('keydown', function(e) {
   console.log(e.which)
   const num = parseInt(e.which || e.detail);
-if (num === code[i]){
+if (code[i] === num){
+  i++
   if (i === code.length -1){
     alert("Bingo");
   }
-  i++
 }
 
 else{
